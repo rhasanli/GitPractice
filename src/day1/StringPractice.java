ef9dd6c5f4b378da6a13e73cb67ed55f91a4d428
rhasanli <ramil@hasanli.net>
2020-03-03 14:07:49 -0600

    test

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -2,7 +2,6 @@ package day1;
 
 public class StringPractice {
     public static void main(String[] args) {
-        System.out.println("Hello Word" +
-                "");
+        System.out.println("Hello Word. This is my success in GitHub setup");
     }
 }
