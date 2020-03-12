17746e5eafa853bf96123453530c779f31600adb
rhasanli <ramil@hasanli.net>
2020-03-11 19:29:36 -0500

    Updated SOUT

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -4,5 +4,6 @@ public class StringPractice {
     public static void main(String[] args)
     {
         System.out.println("Hello Word. GitHub setup");
+        System.out.println("My colors");
     }
 }
