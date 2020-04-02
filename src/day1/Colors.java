248ed2fe5aad4f8caf9dbd378d3da26149342eb3
rhasanli <ramil@hasanli.net>
2020-04-01 19:22:13 -0500

    Added States

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -4,7 +4,7 @@ public class Colors {
     public static void main(String[] args) {
 
         String[] colors = {"red", "green", "blue"};
-        System.out.println("My colors");
-        System.out.println("My colors");
+        System.out.println("My colors.txt");
+        System.out.println("My colors.txt");
     }
 }
